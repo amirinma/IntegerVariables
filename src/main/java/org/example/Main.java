c176413dbcef1bda8f68ee2561eda9fd65591106
ziaro <zia.ro@live.com>
2024-02-13 18:19:50 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        int checking; // declare an int variable named checking.
+        byte miles; //declare a byte variable named miles.
+        short minutes; //declare a short varaible named minutes.
+        long days; //declare a long variable named days;
+
+        checking = -20;
+        miles = 105;
+        minutes = 120;
+        days = 189000;
+        System.out.println("We have made a journy of " + miles + " miles.");
+    }
+}
